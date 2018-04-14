5219ed9d6575af5532b16154522b1ccccd60f575
JotaSprout <jotasprout@gmail.com>
2018-04-14 14:54:08 -0400

    v2-5 Text Boxes

--- textBoxes.js
+++ textBoxes.js
@@ -14,15 +14,16 @@ function nameBoxes(){
     singleRect_2 = getField(thisPagePrefix + "short.ActionText_2");
 }
 
-var singleFullRectangle = [30,595,581,70];
-var topRowRectangle = [30,595,581,335];
-var bottomRowRectangle = [30,330,581,70];
-var leftColumn = [30,595,303,70];
-var rightColumn = [308,595,581,70];
-var leftLower = [30,330,303,70];
-var leftUpper = [30,595,303,335];
-var rightLower = [308,330,581,70];
-var rightUpper = [308,595,581,335];
+// [Upper-Left X, Upper-Left Y, Lower-Right X, Lower-Right Y]
+var singleFullRectangle = [32,551,579,70];
+var topRowRectangle = [32,551,579,313];
+var bottomRowRectangle = [32,308,579,70];
+var leftColumn = [32,551,302,70];
+var rightColumn = [309,551,579,70];
+var leftLower = [32,308,302,70];
+var leftUpper = [32,551,302,313];
+var rightLower = [309,308,579,70];
+var rightUpper = [309,551,579,313];
 
 // Hides the two transparent buttons for choosing number of text boxes
 function hideTextChoices(){
